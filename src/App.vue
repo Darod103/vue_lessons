b1ff984a622eb5c7eed9a5faa2c9b58fb7e0f9d4
darod103 <darod103@gmail.com>
2025-12-31 00:01:18 +1000

    first add linter

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+const text = ref('');
+</script>
+<template>
+<input 
+    type="text"
+    placeholder="Add text"
+    :value="text"
+    @input="e => text = e.target.value">
+<p>{{ text }}</p>
+</template>
+
+<style scoped>
+</style>
